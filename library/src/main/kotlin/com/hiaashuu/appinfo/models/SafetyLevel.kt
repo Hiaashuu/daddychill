db61d9d6307e118a95c1bc6edad813fc06b68fbd
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-21 16:50:15 +0530

    Update via CodXFuse

--- library/src/main/kotlin/com/hiaashuu/appinfo/models/SafetyLevel.kt
+++ library/src/main/kotlin/com/hiaashuu/appinfo/models/SafetyLevel.kt
@@ -2,15 +2,11 @@ package com.hiaashuu.appinfo.models
 
 enum class SafetyLevel {
 
-    /** System will break or become unstable. Never remove. */
     CRITICAL,
 
-    /** Has system dependencies. Remove only if you know the impact. */
     CAUTION,
 
-    /** Generally safe to disable if you don't use the feature. */
     SAFE,
 
-    /** Pre-installed junk with no system role. Safe to remove. */
     BLOATWARE
 }
