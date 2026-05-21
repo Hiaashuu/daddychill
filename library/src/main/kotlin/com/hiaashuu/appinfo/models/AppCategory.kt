f9f27cf327e0d91693b89c7c410679e7ca4cb6fd
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-21 13:47:18 +0530

    Initial commit via CodXFuse

---
+++ library/src/main/kotlin/com/hiaashuu/appinfo/models/AppCategory.kt
@@ -0,0 +1,14 @@
+package com.hiaashuu.appinfo.models
+
+enum class AppCategory {
+    ANDROID_SYSTEM,
+    GOOGLE,
+    GOOGLE_CORE,
+    ONEPLUS_OPLUS,
+    COLOROS_OPPO,
+    QUALCOMM,
+    META_FACEBOOK,
+    MICROSOFT,
+    HEYTAP_NEARME,
+    THIRD_PARTY
+}
