3e690005e42b43c8339bcfda50aa1e54d6a6bd45
Hiaashuu <113388763+Hiaashuu@users.noreply.github.com>
2026-05-21 16:50:17 +0530

    Update via CodXFuse

--- library/src/main/kotlin/com/hiaashuu/appinfo/models/RemovalStatus.kt
+++ library/src/main/kotlin/com/hiaashuu/appinfo/models/RemovalStatus.kt
@@ -2,12 +2,9 @@ package com.hiaashuu.appinfo.models
 
 enum class RemovalStatus {
 
-    /** Do not remove under any circumstance. */
     NO,
 
-    /** Can be removed but test core features after. */
     CAUTION,
 
-    /** Safe to remove. No meaningful system impact. */
     YES
 }
